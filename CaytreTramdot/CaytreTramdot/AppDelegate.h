299ee3e35ff0413a3e5ad500a67003a9d52fd52a
ThanhTung <phanthanhtung.pt@gmail.com>
2013-04-25 15:20:54 +0700

    k

---
+++ CaytreTramdot/CaytreTramdot/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CaytreTramdot
+//
+//  Created by ThanhTung on 4/25/13.
+//  Copyright (c) 2013 THANHTUNG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CaytreTramdot/CaytreTramdot/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  CaytreTramdot
+//
+//  Created by ThanhTung on 4/25/13.
+//  Copyright (c) 2013 THANHTUNG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+@interface ViewController : UIViewController{
+}
+@property (strong, nonatomic) AVAudioPlayer *audioPlayer;
+@end
