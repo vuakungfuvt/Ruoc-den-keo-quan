c5f50c76c94bc14c28ff3b1dea278c1f40e051fd
ThanhTung <phanthanhtung.pt@gmail.com>
2013-05-02 15:24:24 +0700

    ios

--- Graphic/Graphic/MainVC.m
+++ Graphic/Graphic/MainVC.m
@@ -19,6 +19,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        
         self.title = @"ĐỒ THỊ HÀM SỐ";
     }
     return self;
